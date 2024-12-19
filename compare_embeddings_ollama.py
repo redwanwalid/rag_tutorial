d80c2201e3f4f7e9740617b3379f8956f907a560
Sina Khatibi <mr.sina.kh@gmail.com>
2024-12-19 21:51:50 +0100

    fix the issues. create data based ollama is added.

--- compare_embeddings_ollama.py
+++ compare_embeddings_ollama.py
@@ -4,7 +4,7 @@ from scipy.spatial.distance import cosine
 
 def main():
     # Get embedding for a word.
-    embedding_function = OllamaEmbeddings(model="llama3.1")
+    embedding_function = OllamaEmbeddings(model="nomic-embed-text")
     vector1 = embedding_function.embed_query("apple")
     vector2 = embedding_function.embed_query("iphone")
     print(f"Vector for 'apple': {vector1}")
@@ -13,12 +13,6 @@ def main():
     # Calculate cosine similarity
     distance = cosine(vector1, vector2)
     print(f"Cosine distance between 'apple' and 'iphone': {distance}")
-
-    # Compare vector of two words using evaluator
-    evaluator = load_evaluator("pairwise_embedding_distance")
-    words = ("apple", "iphone")
-    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
-    print(f"Comparing ({words[0]}, {words[1]}): {x}")
 
 if __name__ == "__main__":
     main()
